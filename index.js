ecebcf0aed8641c9232cbe8e246417a65755661e
Moshe Kolodny <kolodny@github.com>
2015-12-16 23:43:24 -0500

    added README and fixed some bugs

--- index.js
+++ index.js
@@ -10,23 +10,34 @@ var readVersion = function(filename) {
     fs.readFile(filename, function(err, buf) {
       if (err) return reject(err);
       var pkg = JSON.parse(buf.toString());
-      return resolve({name: pkg.name, license: pkg.license});
+      if (!pkg.name || !pkg.license) return resolve(null);
+      var license = pkg.license.type || pkg.license;
+      return resolve({name: pkg.name, license: license});
     });
   });
 }
 
 module.exports = function() {
-  return globAsync('**/package.json')
+  return globAsync('**/node_modules/*/package.json')
     .then(function(filenames) {
       return Promise.all(filenames.map(readVersion))
     })
     .then(function(infos) {
       return infos.reduce(function(acc, info) {
-        if (!acc[info.license]) {
-          acc[info.license] = [];
+        if (info) {
+          if (!acc[info.license]) {
+            acc[info.license] = {};
+          }
+          acc[info.license][info.name] = true;
         }
-        acc[info.license].push(info.name);
+        return acc;
+      }, {});
+    })
+    .then(function(resultsAsMap) {
+      return Object.keys(resultsAsMap).reduce(function(acc, license) {
+        acc[license] = Object.keys(resultsAsMap[license]).sort();
         return acc;
       }, {});
     });
+    ;
 };
